815df6778972fde3e863f78bfe9096a70dff9b23
haruki7049 <tontonkirikiri@gmail.com>
2024-08-15 22:59:34 +0900

    chore: Update buildJanetPackage

--- buildJanetPackage.nix
+++ buildJanetPackage.nix
@@ -11,5 +11,21 @@ in
     }:
     stdenv.mkDerivation {
       inherit pname version src;
+
+      buildInputs = [
+        pkgs.janet
+        pkgs.jpm
+      ];
+
+      JANET_LIBPATH = "${pkgs.janet}/lib";
+
+      buildPhase = ''
+        jpm build
+      '';
+
+      installPhase = ''
+        mkdir -p $out/bin
+        install -m755 build/${pname} $out/bin/${pname}-${version}
+      '';
     };
 }
