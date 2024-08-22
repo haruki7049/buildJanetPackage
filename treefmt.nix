31e4089b232a434365d7cf41333fdf511eae7477
haruki7049 <tontonkirikiri@gmail.com>
2024-08-22 16:12:03 +0900

    ci: Enable actionlint

--- treefmt.nix
+++ treefmt.nix
@@ -2,6 +2,7 @@
   projectRootFile = "treefmt.nix";
   programs.nixpkgs-fmt.enable = true;
   programs.shellcheck.enable = true;
+  programs.actionlint.enable = true;
 
   settings.formatter.shellcheck.excludes = [ ".envrc" ];
 }
