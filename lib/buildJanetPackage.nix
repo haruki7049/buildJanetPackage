5ff1c84dce9947667ba436664b048d927da8d47e
haruki7049 <tontonkirikiri@gmail.com>
2024-09-16 16:20:07 +0900

    feat(buildJanetPackage.nix): Update

--- lib/buildJanetPackage.nix
+++ lib/buildJanetPackage.nix
@@ -1,24 +1,69 @@
 { pkgs }:
 
-let
-  stdenv = pkgs.stdenv;
-in
 {
   buildJanetPackage =
     {
       pname,
       version,
       src,
+      depsFile,
+      deps ? import depsFile,
     }:
+    let
+      stdenv = pkgs.stdenv;
+      lib = pkgs.lib;
+
+      deps-fetcher = { name, url, hash }: stdenv.mkDerivation {
+        inherit name;
+
+        src = pkgs.fetchurl {
+          inherit url hash;
+        };
+
+        buildInputs = [
+          pkgs.janet
+          pkgs.jpm
+        ];
+
+        buildPhase = ''
+          jpm install --local
+        '';
+
+        installPhase = ''
+          mkdir -p $out/lib
+          cp -r jpm_tree/lib/* $out/lib
+        '';
+      };
+
+      vendorSet = builtins.listToAttrs (map (v: { name = v.name; value = deps-fetcher { inherit (v) name url hash; }; }) deps);
+
+      vendorPaths = map (v: v.outPath) (builtins.attrValues vendorSet);
+
+      vendor = stdenv.mkDerivation {
+        name = "vendor";
+
+        src = lib.cleanSource ./.;
+
+        dontBuild = true;
+
+        installPhase = ''
+          mkdir -p $out/lib
+          ${lib.strings.concatLines (map (v: "cp -r ${v}/lib/* $out/lib") vendorPaths)}
+        '';
+      };
+    in
     stdenv.mkDerivation {
       inherit pname version src;
 
       buildInputs = [
         pkgs.janet
         pkgs.jpm
+        vendor
       ];
 
+      JANET_PATH = "${pkgs.janet}/lib";
       JANET_LIBPATH = "${pkgs.janet}/lib";
+      JANET_MODPATH = "${vendor}/lib";
 
       buildPhase = ''
         jpm build
