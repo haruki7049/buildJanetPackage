5d1ce8761273e8eff48f9591adb38f16cbfcc4b7
haruki7049 <tontonkirikiri@gmail.com>
2024-09-19 08:57:08 +0900

    test(example-deps-parser): Edit hash

--- examples/deps-parser/deps.nix
+++ examples/deps-parser/deps.nix
@@ -6,7 +6,7 @@
     src = fetchgit {
       url = "https://github.com/janet-lang/spork";
       rev = "253a67e89dca695632283ef60f77851311c404c9";
-      hash = "sha256-I5cOrJ9/3p2LAimQxnJR6etdxvLuZFfundOucgz5nok=";
+      hash = "sha256-+XZaTThLm75IsifMM0IAPasZwCv42MmI9+e2sy+jl1o=";
     };
     deps = [ ];
   }
@@ -15,7 +15,7 @@
     src = fetchgit {
       url = "https://github.com/janet-lang/jpm";
       rev = "d93b7c243645d31410a81fb9ab8f7a5e5608f0d0";
-      hash = "sha256-MslOSQ2ZOajk7GHU+lHQM1tznswjkzC8mQc6c3QpPIA=";
+      hash = "sha256-dXu6r+mSS3p7nsOmwfoJH2Sc9okU1S7wYRswdEmSJfg=";
     };
     deps = [ ];
   }
