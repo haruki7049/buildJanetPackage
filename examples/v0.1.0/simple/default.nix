c03f9a3ca413d671a26dfd1ec4294d553438e74e
haruki7049 <tontonkirikiri@gmail.com>
2024-09-19 11:04:14 +0900

    fix(example-v0.1.0-simple): Add hash

--- examples/v0.1.0/simple/default.nix
+++ examples/v0.1.0/simple/default.nix
@@ -3,7 +3,10 @@
     import
       (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/36a9aeaaa17a2d4348498275f9fe530cd4f9e519.tar.gz")
       { },
-  buildJanetPackage-source ? builtins.fetchTarball "https://github.com/haruki7049/buildJanetPackage/archive/refs/tags/0.1.0.tar.gz",
+  buildJanetPackage-source ? builtins.fetchTarball {
+    url = "https://github.com/haruki7049/buildJanetPackage/archive/refs/tags/0.1.0.tar.gz";
+    sha256 = "17pwy76lv0jn6ynm1051zlfcj4lk5q108wn4i69d7m0jgrwpw7nb";
+  },
 }:
 
 let
