2ce5f911f510761896ba07ae0bdd4a94c6aa8007
haruki7049 <tontonkirikiri@gmail.com>
2024-09-19 11:37:37 +0900

    fix(example-janet-lsp): Edit hash

--- examples/unstable/janet-lsp/deps.nix
+++ examples/unstable/janet-lsp/deps.nix
@@ -6,7 +6,7 @@
     src = fetchgit {
       url = "https://github.com/janet-lang/spork";
       rev = "253a67e89dca695632283ef60f77851311c404c9";
-      hash = "sha256-OnfD1w4KCKAQ101+NehSWOdDGgHjhdctb9AXHNzo/28=";
+      hash = "sha256-+XZaTThLm75IsifMM0IAPasZwCv42MmI9+e2sy+jl1o=";
     };
     deps = [ ];
   }
@@ -15,7 +15,7 @@
     src = fetchgit {
       url = "https://github.com/CFiggers/cmd";
       rev = "5b9d97d404733dfc32b35c7683940f7031de4bf1";
-      hash = "sha256-ZyPpPNF/wtjQcxVrre+KwmfghH31dK1f9jTDH4htn4U=";
+      hash = "sha256-N4ct9H/0sx//2ml/b4HaY57FF7Dit1ogD0NuRbXmu04=";
     };
     deps = [ ];
   }
@@ -24,7 +24,7 @@
     src = fetchgit {
       url = "https://github.com/CFiggers/jayson";
       rev = "4f54041617340c8ff99bc1e6b285b720184965e2";
-      hash = "sha256-5HduXYKq4okuaFrnVfGOEOtLLabVYasANsHFOU2lC00=";
+      hash = "sha256-961JRjy/JB0mDGTVbMofBf6vwu95TcUnnb7GxYkQ9EI=";
     };
     deps = [ ];
   }
@@ -33,7 +33,7 @@
     src = fetchgit {
       url = "https://github.com/ianthehenry/judge";
       rev = "10754df781b34068e05291951db786933ec6b681";
-      hash = "sha256-Idl0I+sXV8/XsRme2qo7wPHbJhUyRZ+1xbyGRiziot4=";
+      hash = "sha256-1zvr1WsllZFeKPb4EgdvTpp67lo1T/M8dEXGNHzOHj0=";
     };
     deps = [
       {
